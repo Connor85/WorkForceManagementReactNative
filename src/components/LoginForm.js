e54f500905e9b1db7f9006db649e0a843fab79ff
Connor McCarthy <connormccarthy8@gmail.com>
2019-02-03 19:12:16 -0800

    Connect email field to reducer

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,44 @@
 import React, { Component } from "react";
-import { Card, Input, Button, CardSection } from "./common";
+import { connect } from "react-redux";
+import { emailChanged } from "../actions";
+import { Card, CardSection, Input, Button } from "./common";
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
   render() {
     return (
       <Card>
-        <Card>
-          <CardSection>
-            <Input label="Email" placeholder="email@gmail.com" />
-          </CardSection>
-          <CardSection>
-            <Input
-              secureTextEntry
-              label="Password"
-              placeholder="YourDogsName"
-            />
-          </CardSection>
-          <CardSection>
-            <Button>Login</Button>
-          </CardSection>
-        </Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input secureTextEntry label="Password" placeholder="password" />
+        </CardSection>
+
+        <CardSection>
+          <Button>Login</Button>
+        </CardSection>
       </Card>
     );
   }
 }
 
-export default LoginForm;
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { emailChanged }
+)(LoginForm);
